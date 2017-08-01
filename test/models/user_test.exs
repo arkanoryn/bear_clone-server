900ad599317598c1398ef8252432790c06b0d468
Jean Pignouf <mfouchardiere@avarteq.de>
2017-08-01 09:59:46 +0000

    Added tests for CUD

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -15,4 +15,38 @@ defmodule BearClone.UserTest do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "Create a User" do
+    assert {:ok, _user} = BearClone.User.create(@valid_attrs)
+
+    assert {:error, %{valid?: false}} = BearClone.User.create(@valid_attrs)
+  end
+
+  test "Creating an Invalid user" do
+    assert {:error, %{valid?: false}} = BearClone.User.create(@invalid_attrs)
+  end
+
+  test "Deleting a User" do
+    assert {:ok, user} = BearClone.User.create(@valid_attrs)
+    assert {:ok, same_user} = BearClone.User.delete(user.id)
+
+    assert %{__meta__: %{state: :loaded}} = user
+    assert %{__meta__: %{state: :deleted}} = same_user
+  end
+
+  test "Deleting a deleted or invalid User" do
+    assert {:ok, user} = BearClone.User.create(@valid_attrs)
+    assert {:ok, _same_user} = BearClone.User.delete(user.id)
+
+    string = "[GET_BY_ID] - User #{user.id} not found"
+    assert {:error, string} == BearClone.User.delete(user.id)
+  end
+
+  test "Update user" do
+    assert {:ok, user} = BearClone.User.create(@valid_attrs)
+
+    assert {:ok, user_too} = BearClone.User.update(%{username: "Chuck Norris"}, user.id)
+    assert user.id == user_too.id
+    refute user.username == user_too.username
+  end
 end
